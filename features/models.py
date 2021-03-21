a30ea71e39d28cb0ecffd45bf30cf909dd1ac30b
Gauravi Sharma <gauravisharma1508@gamil.com>
2021-03-21 20:10:52 +0530

    Done

---
+++ features/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Members(models.Model):
+    username=models.CharField(max_length=250)
+    password=models.CharField(max_length=16)
+    email=models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.username
+
+
